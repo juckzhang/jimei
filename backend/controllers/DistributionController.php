96bf73e42dcb0632096fd766b626d5b531299677
张超 <zhangchao12@staff.sina.com.cn>
2020-08-29 17:28:28 +0800

    fix bug

--- backend/controllers/DistributionController.php
+++ backend/controllers/DistributionController.php
@@ -16,7 +16,7 @@ class DistributionController extends BaseController
         $_prePage  = ArrayHelper::getValue($this->paramData,'numPerPage');
         $_page       = ArrayHelper::getValue($this->paramData,'pageNum');
         $_keyWord  = ArrayHelper::getValue($this->paramData,'keyword');
-        $data = DistributionService::getService()->DistributionlList($_keyWord,$_page,$_prePage);
+        $data = DistributionService::getService()->DistributionList($_keyWord,$_page,$_prePage);
         return $this->render('distribution-list',$data);
     }
 
--- backend/controllers/OrderController.php
+++ backend/controllers/OrderController.php
@@ -16,7 +16,7 @@ class OrderController extends BaseController
         $_prePage  = ArrayHelper::getValue($this->paramData,'numPerPage');
         $_page       = ArrayHelper::getValue($this->paramData,'pageNum');
         $_keyWord  = ArrayHelper::getValue($this->paramData,'keyword');
-        $data = OrderService::getService()->OrderlList($_keyWord,$_page,$_prePage);
+        $data = OrderService::getService()->OrderList($_keyWord,$_page,$_prePage);
         return $this->render('order-list',$data);
     }
 
