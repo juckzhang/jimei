4dbcac4bd6ffa1b43a7f65118e1a4e1d3b2a5cd8
张超 <zhangchao12@staff.sina.com.cn>
2020-09-10 22:10:34 +0800

    新增接口

--- backend/views/customer/edit-customer.php
+++ backend/views/customer/edit-customer.php
@@ -2,46 +2,22 @@
 use yii\helpers\Url;
 use yii\helpers\ArrayHelper;
 ?>
-<h2 class="contentTitle">编辑机型</h2>
+<h2 class="contentTitle">编辑客户</h2>
 <div class="pageContent">
-    <form method="post" action="<?=Url::to(['phone/edit-phone','id' => ArrayHelper::getValue($model,'id','')])?>" class="pageForm required-validate" onsubmit="return validateCallback(this,dialogAjaxDone)">
+    <form method="post" action="<?=Url::to(['customer/edit-customer','id' => ArrayHelper::getValue($model,'id','')])?>" class="pageForm required-validate" onsubmit="return validateCallback(this,dialogAjaxDone)">
         <div class="pageFormContent nowrap" layoutH="97">
             <dl>
                 <dt>名称：</dt>
                 <dd>
-                    <input type="text" name="PhoneModel[modal]" maxlength="20" class="required" value="<?=ArrayHelper::getValue($model,'modal','')?>"/>
+                    <input type="text" name="CustomerModel[name]" maxlength="20" class="required" value="<?=ArrayHelper::getValue($model,'name','')?>"/>
                     <span class="info">名称不能为空</span>
-                </dd>
-            </dl>
-            <dl>
-                <dt>品牌分类：</dt>
-                <dd>
-                    <select name="PhoneModel[brand_id]" value="<?=ArrayHelper::getValue($model, 'brand_id')?>">
-                        <?php foreach($brandList as $brand):?>
-                            <option value="<?=$brand['id']?>" <?=ArrayHelper::getValue($model, 'brand_id') == $brand['id'] ? 'selected' : ''?>><?=$brand['name']?></option>
-                        <?php endforeach;?>
-                    </select>
                 </dd>
             </dl>
             <dl>
                 <dt>条码：</dt>
                 <dd>
-                    <input type="text" name="PhoneModel[barcode]" maxlength="20" class="required" value="<?=ArrayHelper::getValue($model,'barcode','')?>"/>
+                    <input type="text" name="CustomerModel[barcode]" maxlength="20" class="required" value="<?=ArrayHelper::getValue($model,'barcode','')?>"/>
                     <span class="info">条码</span>
-                </dd>
-            </dl>
-            <dl>
-                <dt>宽：</dt>
-                <dd>
-                    <input type="text" name="PhoneModel[width]" maxlength="20" class="required" value="<?=ArrayHelper::getValue($model,'width',0)?>"/>
-                    <span class="info">宽不能为空</span>
-                </dd>
-            </dl>
-            <dl>
-                <dt>高：</dt>
-                <dd>
-                    <input type="text" name="PhoneModel[height]" maxlength="20" class="required" value="<?=ArrayHelper::getValue($model,'height',0)?>"/>
-                    <span class="info">高不能为空</span>
                 </dd>
             </dl>
         </div>
