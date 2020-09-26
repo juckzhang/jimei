991913aac9f29b460b03fa0f092e89bb8b87169a
张超 <zhangchao12@staff.sina.com.cn>
2020-09-26 21:24:14 +0800

    样式调整

--- backend/views/phone/relation-list.php
+++ backend/views/phone/relation-list.php
@@ -51,7 +51,7 @@ $search = ArrayHelper::getValue($params,'search');
             <th width="80">材质名称</th>
             <th width="80">左边距</th>
             <th width="80">上边距</th>
-            <th width="80">圆角边素材</th>
+            <th width="80">上框图链接</th>
             <th class="<?=$orderDirection?>" style="cursor: pointer;" orderfield="update_time" width="80">修改时间</th>
             <th width="70">操作</th>
         </tr>
