a21efd7a784191335c972fb2c3ee2dfde9fe6026
张超 <zhangchao12@staff.sina.com.cn>
2020-10-01 20:58:40 +0800

    添加搜索查询条件

--- backend/views/phone/phone-list.php
+++ backend/views/phone/phone-list.php
@@ -41,6 +41,7 @@ $canvasType = ['1' => '普通画布', '2' => '大画布'];
                 </table>
                 <div class="subBar">
                     <ul>
+                        <li><div class="button"><div class="buttonContent"><button type="reset">重置</button></div></div></li>
                         <li><div class="buttonActive"><div class="buttonContent"><button type="submit">检索</button></div></div></li>
                         <?php if($search):?>
                         <li><div class="button"><div class="buttonContent"><button type="button" multLookup="ids[]" warn="请选择部门">选择带回</button></div></div></li>
