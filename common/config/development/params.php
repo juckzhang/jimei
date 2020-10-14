ac0796628c80bae92b569b73359ce347300a3090
张超 <zhangchao12@staff.sina.com.cn>
2020-10-14 17:01:58 +0800

    feat/search

--- common/config/development/params.php
+++ common/config/development/params.php
@@ -1,5 +1,7 @@
 <?php
 return [
     'imageUrlPrefix' => 'https://phonecases.dongbin.dev/',
-    'domain'       => 'http://106.12.72.118/'
+    'domain'       => 'http://106.12.72.118/',
+    'appKey'       => '200305112610017',
+    'appSecret'    => '8348bac6ef3e4b3e8de26c83a5e75da3',
 ];
--- common/config/product/params.php
+++ common/config/product/params.php
@@ -1,5 +1,7 @@
 <?php
 return [
     'imageUrlPrefix' => 'http://www.wolongtech.com/',
-    'domain'       => 'http://api.kungchinese.com/'
+    'domain'       => 'http://api.kungchinese.com/',
+    'appKey'       => '200428181312266',
+    'appSecret'    => '81edd4791db141b386f066ffda128a38',
 ];
