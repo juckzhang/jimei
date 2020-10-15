211d4d19662898472df8d572a9479e1ad4bcc5d8
张超 <zhangchao12@staff.sina.com.cn>
2020-10-15 10:12:02 +0800

    feat/search

--- common/config/product/params.php
+++ common/config/product/params.php
@@ -4,4 +4,5 @@ return [
     'domain'       => 'http://api.kungchinese.com/',
     'appKey'       => '200428181312266',
     'appSecret'    => '81edd4791db141b386f066ffda128a38',
+    'apiUrl'       => 'https://openapi.gjpqqd.com/Service/ERPService.asmx/ERPApi',
 ];
