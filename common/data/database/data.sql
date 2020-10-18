3cbd898ac74d1a1465b03281a7daf6b857f9dc2b
张超 <zhangchao12@staff.sina.com.cn>
2020-10-18 19:32:44 +0800

    feat/search

--- common/data/database/data.sql
+++ common/data/database/data.sql
@@ -71,8 +71,7 @@ CREATE TABLE if NOT EXISTS jimei_order(
   suitecode varchar(255) not null default '' comment '套餐编码',
   create_time bigint unsigned NOT NULL DEFAULT 0 comment'创建时间',
   update_time bigint unsigned NOT NULL DEFAULT 0 comment'修改时间',
-  status tinyint unsigned NOT NULL DEFAULT 0 comment'状态 0：有效 1：删除',
-  unique (order_id,base_id, suitecode)
+  status tinyint unsigned NOT NULL DEFAULT 0 comment'状态 0：有效 1：删除'
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 auto_increment=1 comment'订单';
 
 -- 机型材质
