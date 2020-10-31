0631b46f3f26269c2fc62d2466a202f78b74c49e
张超 <zhangchao12@staff.sina.com.cn>
2020-10-31 14:14:52 +0800

    feat/search

--- backend/views/meal/meal-list.php
+++ backend/views/meal/meal-list.php
@@ -36,7 +36,7 @@ $notMore = ArrayHelper::getValue($params, 'notMore');
                         </td>
                         <td>
                             机型:
-                            <input type="hidden" name="other[mobile_id]" data-name="phone.id" value="<?=ArrayHelper::getValue($other, 'phone_id')?>">
+                            <input type="hidden" name="other[mobile_id]" data-name="phone.id" value="<?=ArrayHelper::getValue($other, 'mobile_id')?>">
                             <input type="text" class="textInput readonly" readonly="true" name="phone-name" value="<?=ArrayHelper::getValue($params,'phone-name')?>" data-name="phone.name" suggestfields="name" lookupgroup="phone" autocomplete="off">
                             <a id="_look-mobile" class="btnLook" href="<?=Url::to(['phone/phone-list', 'search' => 1])?>" lookupgroup="phone">查找带回</a>
                         </td>
