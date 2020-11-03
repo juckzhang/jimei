82c285ce6e5dbf8eb3efed2262ebf7796944b69b
张超 <zhangchao12@staff.sina.com.cn>
2020-11-03 16:15:04 +0800

    feat/search

--- backend/services/MaterialService.php
+++ backend/services/MaterialService.php
@@ -1,6 +1,7 @@
 <?php
 namespace backend\services;
 
+use common\models\mysql\ColorMaterialModel;
 use common\models\mysql\MaterialModel;
 use backend\services\base\BackendService;
 use yii\helpers\ArrayHelper;
@@ -31,7 +32,36 @@ class MaterialService extends BackendService
 
     public function editMaterial($data){
         $id = ArrayHelper::getValue($data, 'id');
+        $model = $this->editInfo($id, MaterialModel::className());
 
+        //获取颜色
+        $colorIds = array_unique(explode(',', $model->color_id));
+
+        $filed = ['color_id', 'material_id', 'create_time', 'update_time',];
+        $batchData = [];
+        $now = time();
+
+        foreach ($colorIds as $colorId){
+            $batchData[] = [
+                'color_id' => $colorId,
+                'material_id' => $model->material_id,
+                'create_time' => $now,
+                'update_time' => $now,
+            ];
+        }
+
+        if($batchData){
+            $transaction = \Yii::$app->db->beginTransaction();
+            try{
+                ColorMaterialModel::deleteAll(['material_id' => $model->id]);
+                \Yii::$app->db->createCommand()->batchInsert(ColorMaterialModel::tableName(),$filed,$batchData)->execute();
+                $transaction->commit();
+            }catch (\Exception $e){
+                $transaction->rollBack();
+            }
+        }
+
+        return $model;
     }
 }
 
