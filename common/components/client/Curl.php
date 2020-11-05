5800fb0a34d75f85f1e565f9848210368885d38a
张超 <zhangchao12@staff.sina.com.cn>
2020-11-05 18:49:23 +0800

    feat/search

--- common/components/client/Curl.php
+++ common/components/client/Curl.php
@@ -6,9 +6,9 @@ class Curl extends Handle{
     public static function sCurl($option) {
         $chObj          = new self($option);
         $ch             = $chObj->get();
-        $starttime      = microtime(true);
-        $result         = curl_exec($ch);
-        $endtime        = microtime(true);
+        $starttime      = \microtime(true);
+        $result         = \curl_exec($ch);
+        $endtime        = \microtime(true);
 
         //记录日志信息
         $logInfo = [
@@ -19,16 +19,16 @@ class Curl extends Handle{
             'result' => $result,
         ];
 
-        if (curl_errno($ch)) {
+        if (\curl_errno($ch)) {
             $result     = false;
-            $err        = curl_error($ch);
+            $err        = \curl_error($ch);
             $logInfo['errMessage'] = $err;
 //            Log::error($logInfo, 'task-collector');
         }else{
 //            Log::debug($logInfo, 'task-collector');
         }
 
-        curl_close($ch);
+        \curl_close($ch);
 
         return $result;
     }
@@ -45,47 +45,47 @@ class Curl extends Handle{
         $map            = array();
         $errorMsg       = array();
 
-        $mch            = curl_multi_init();
+        $mch            = \curl_multi_init();
         foreach ($options as $i => $opt) {
             $chObj      = new self($opt);
             $ch         = $chObj->get();
-            curl_multi_add_handle ($mch, $ch);
+            \curl_multi_add_handle ($mch, $ch);
             $map[$i]    = $ch;
         }
         # execute the handles
         $active = null;
         do {
-            $mrc = curl_multi_exec($mch, $active);
+            $mrc = \curl_multi_exec($mch, $active);
         } while ($mrc == CURLM_CALL_MULTI_PERFORM);
 
         while ( $active && $mrc == CURLM_OK ) {
-            if (curl_multi_select($mch, 0.8) == - 1) {      # select超时返回0,失败返回-1,当超时的时候setOpt中的timeout就起作用了,使用man select 查看实现原理
+            if (\curl_multi_select($mch, 0.8) == - 1) {      # select超时返回0,失败返回-1,当超时的时候setOpt中的timeout就起作用了,使用man select 查看实现原理
                 usleep(100);
             }
             do {
-                $mrc = curl_multi_exec($mch, $active);
+                $mrc = \curl_multi_exec($mch, $active);
             } while ( $mrc == CURLM_CALL_MULTI_PERFORM );
         }
         # get results
         foreach ($map as $k => $handle) {
             if (curl_error($handle) == '') {
-                $temp   = curl_multi_getcontent($handle);
+                $temp   = \curl_multi_getcontent($handle);
                 $count  = 0;
                 while (!$temp) {
                     if ($count > 3)
                         break;
                     usleep (100);
-                    $temp = curl_multi_getcontent ($handle);
+                    $temp = \curl_multi_getcontent ($handle);
                     $count ++;
                 }
                 $result[$k] = $temp;
-                curl_multi_remove_handle($mch, $handle);
-                curl_close ($handle);
+                \curl_multi_remove_handle($mch, $handle);
+                \curl_close ($handle);
             } else {
                 $errorMsg[$options[$k]['url']] = curl_error($handle);
             }
         }
-        curl_multi_close($mch);
+        \curl_multi_close($mch);
 
         if(!empty($errorMsg)){
             $errInfo            = empty($errorMsg) ? '' : serialize($errorMsg);
--- common/components/client/Handle.php
+++ common/components/client/Handle.php
@@ -6,7 +6,7 @@ abstract class Handle {
     private $ch                = null;
 
     public function __construct($option) {
-        $this->ch = curl_init();
+        $this->ch = \curl_init();
         $this->setOpt($option);
     }
 
@@ -21,27 +21,27 @@ abstract class Handle {
                 $option['url'] .= strpos($option['url'], '?') ? '&'.$key.'='.rawurlencode($value) : '?'.$key.'='.rawurlencode($value);
             }
     
-        curl_setopt($this->ch, CURLOPT_URL, $option['url']);
-        curl_setopt($this->ch, CURLOPT_HEADER, false);
-        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        \curl_setopt($this->ch, CURLOPT_URL, $option['url']);
+        \curl_setopt($this->ch, CURLOPT_HEADER, false);
+        \curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        \curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        \curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         $ctimeout = isset($option['ctimeout']) ? $option['ctimeout'] : 1;
         $timeout  = isset($option['timeout']) ? $option['timeout'] : 1;
         $version  = curl_version();
         if ($version['version_number'] >= self::CURL_BASE_VERSION){
-            curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT_MS, $ctimeout * 1000);
-            curl_setopt($this->ch, CURLOPT_NOSIGNAL, 1); 
-            curl_setopt($this->ch, CURLOPT_TIMEOUT_MS, $timeout * 1000);
+            \curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT_MS, $ctimeout * 1000);
+            \curl_setopt($this->ch, CURLOPT_NOSIGNAL, 1);
+            \curl_setopt($this->ch, CURLOPT_TIMEOUT_MS, $timeout * 1000);
         } else {
             $ctimeout    = 1;
             $timeout     = ($timeout >= 1)? intval($timeout) : 1;
-            curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, $ctimeout);
-            curl_setopt($this->ch, CURLOPT_TIMEOUT, $timeout);    //执行超时
+            \curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, $ctimeout);
+            \curl_setopt($this->ch, CURLOPT_TIMEOUT, $timeout);    //执行超时
         }
         switch ($method) {
         case 'POST' :
-            curl_setopt($this->ch, CURLOPT_POST, true);
+            \curl_setopt($this->ch, CURLOPT_POST, true);
             if (isset($option['args']) && $option['args']) {
                 if(isset($option['contentJson'])){
                     if(!isset($option['header']) or !is_array($option['header'])) $option['header'] = [];
@@ -49,19 +49,19 @@ abstract class Handle {
                     $postData = json_encode($option['args']);
                     $option['header'][] = 'Content-Length: '.strlen($postData);
 
-                    curl_setopt($this->ch, CURLOPT_POSTFIELDS, $postData);
+                    \curl_setopt($this->ch, CURLOPT_POSTFIELDS, $postData);
                 }else
-                    curl_setopt($this->ch, CURLOPT_POSTFIELDS, http_build_query($option['args']));
+                    \curl_setopt($this->ch, CURLOPT_POSTFIELDS, http_build_query($option['args']));
             }
             break;
         case 'DELETE' :
-            curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            \curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
             break;
         }
         if (isset($option['cookie']))
-            curl_setopt($this->ch, CURLOPT_COOKIE, $option['cookie']);
+            \curl_setopt($this->ch, CURLOPT_COOKIE, $option['cookie']);
     
         if (isset($option['header']))
-            curl_setopt($this->ch, CURLOPT_HTTPHEADER, $option['header']);
+            \curl_setopt($this->ch, CURLOPT_HTTPHEADER, $option['header']);
     }
 }
