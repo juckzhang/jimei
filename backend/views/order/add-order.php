b9bf13dd7bf6b50ee66d71e2d0c03903a847ce76
张超 <zhangchao12@staff.sina.com.cn>
2020-11-23 10:27:42 +0800

    feat/search

--- backend/views/order/add-order.php
+++ backend/views/order/add-order.php
@@ -6,7 +6,7 @@ $params = \Yii::$app->request->getPost();
 ?>
 <h2 class="contentTitle">添加订单</h2>
 <div class="pageContent nowrap">
-    <form method="post" action="<?=Url::to(['order/add-order'])?>" class="pageForm required-validate" onsubmit="return validateCallback(this,submitCallBack)">
+    <form method="post" action="<?=Url::to(['order/add-order'])?>" class="pageForm required-validate" onsubmit="return validateCallback(this,dialogAjaxDone)">
         <input type="hidden" name="base_id" value="<?=ArrayHelper::getValue($params,'base_id')?>"/>
         <div class="pageFormContent" layoutH="97">
             <dl>
