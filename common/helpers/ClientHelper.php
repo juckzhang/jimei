6b04e6e4cb697008566f93067affab7070cb8559
marmot <zhangchao12@staff.weibo.com>
2021-09-13 21:01:01 +0800

    Update ClientHelper.php

--- common/helpers/ClientHelper.php
+++ common/helpers/ClientHelper.php
@@ -10,7 +10,8 @@ use yii\httpclient\Client;
 class ClientHelper
 {
     const APP_KEY = '200305112610017';
-    const APP_SECRET = '8348bac6ef3e4b3e8de26c83a5e75da3';
+    // const APP_SECRET = '8348bac6ef3e4b3e8de26c83a5e75da3';
+    const APP_SECRET = '32ba85dbdf7c401f9f9d5bb3f42b3cfe';
     const VERSION = '1.0';
     const URL_API = 'http://local.gjpqqd.com:5918/Service/ERPService.asmx/ERPApi';
 
@@ -40,33 +41,7 @@ class ClientHelper
         return $param;
     }
 
-    private function sCurl_bak($param, $data){
-        $url = \Yii::$app->params['apiUrl'].'?'.http_build_query($param);
-        $response = (new Client(
-            [
-                'responseConfig' => [
-                    'format' => Client::FORMAT_JSON,
-                ],
-            ]
-        ))->createRequest()
-            ->setMethod('post')
-            ->setData($data)
-            ->setUrl($url)
-            ->setFormat(Client::FORMAT_JSON)
-            ->setHeaders(['Content-Type'=>'application/json'])
-            ->send();
-
-        \Yii::$app->bizLog->log([
-            'url' => $url,
-            'body' => $data,
-            'result' => $response->data,
-        ], 'curl', 'Info');
-        if($response->isOk) return $response->data;
-
-        return [];
-    }
-
-    private function sCurl($param, $data){
+    private static function sCurl($param, $data){
         $url = \Yii::$app->params['apiUrl'].'?'.http_build_query($param);
         $option = [
             'url' => $url,
@@ -86,6 +61,7 @@ class ClientHelper
         return [];
     }
 
+    //同步套餐信息
     public static function rsyncMeal($data){
         $param = static::parseParam('erp.goodssuite.sync', $data);
         $result = static::sCurl($param, $data);
@@ -123,8 +99,44 @@ class ClientHelper
         return $ret;
     }
 
+    //获取配货单列表
     public static function rsyncOrder($data){
         $param = static::parseParam('erp.dist.get', $data);
+        $result = static::sCurl($param, $data);
+
+        if(ArrayHelper::getValue($result, 'code') == 0){
+            return $result;
+        }
+
+        return [];
+    }
+
+    /**
+     * 获取预扣款订单列表
+     * pageno	当前页数	Y	int	从1开始
+     * pagesize	每页数据条数	Y	int	最大值100
+     * starttime	制单时间开始	Y	datatime	
+     * endtime	制单时间结束	Y	datatime	
+     * orderstatus	单据状态	Y	string(25)	audit=审核、financeaudit=财审、send=发货
+     */
+    public static function orderList($data){
+        $param = static::parseParam('erp.jqmorder.get', $data);
+        $result = static::sCurl($param, $data);
+
+        if(ArrayHelper::getValue($result, 'code') == 0){
+            return $result;
+        }
+
+        return [];
+    }
+
+    /**
+     * 财审接口
+     * billNoList	全渠道订单ID集合	Y	array
+     */
+    public static function FunctionName($data)
+    {
+        $param = static::parseParam('erp.order.financeaudit', $data);
         $result = static::sCurl($param, $data);
 
         if(ArrayHelper::getValue($result, 'code') == 0){
