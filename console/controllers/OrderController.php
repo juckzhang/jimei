0f7932113c1d1eb7200ae514543e3162f6a129f1
juckzhang <zhangchao12@staff.weibo.com>
2021-09-14 10:02:33 +0800

    feat/erqi

--- console/controllers/OrderController.php
+++ console/controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace console\controllers;
 
 use common\models\mysql\DistributionModel;
 use common\models\mysql\OrderModel;
+use common\helpers\ClientHelper;
 
 class OrderController extends BaseController{
 
@@ -24,4 +25,18 @@ class OrderController extends BaseController{
 
         return true;
     }
+
+    public function actionRsyncOrder($startTime = null, $endTime = null)
+    {
+        $data = ClientHelper::orderList([
+            'pageno' => 1,
+            'pagesize' => 100,
+            'orderstatus' => 'audit',
+            'starttime' => date('Y-m-d H:i:s', strtotime("-1days")),
+            'endtime' => date('Y-m-d H:i:s'),
+        ]);
+
+        echo json_encode($data);
+    }
+
 }
